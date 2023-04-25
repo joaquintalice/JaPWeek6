59e43353789d176af0b24320fd74861c5adb667f
Joaquin Talice <joaquintalice@gmail.com>
2023-04-25 00:18:16 -0300

    Logic fix - Upgrade
    -Now, the user can select the index of the list to remove an element

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-/* ***************************** */ 
-/* Acá se vinculan los elementos del HTML para ser modificados en el programa */ 
-const btnAgregar = document.getElementById("btn-agregar"); 
+/* ***************************** */
+/* Acá se vinculan los elementos del HTML para ser modificados en el programa */
+const btnAgregar = document.getElementById("btn-agregar");
 const btnEliminar = document.getElementById("btn-eliminar");
-const statusInfo = document.getElementById("status-info"); 
-const ulContainer = document.getElementById("ul-container"); 
-/* ***************************** */ 
+const statusInfo = document.getElementById("status-info");
+const ulContainer = document.getElementById("ul-container");
+/* ***************************** */
 
 
 
 const tasks = []; // Variable que se inicializa en un array vacío
 
-/* ***************************** */ 
+/* ***************************** */
 btnAgregar.addEventListener("click", addTask); // Se agrega un event listener al botón de agregar
 btnEliminar.addEventListener("click", deleteTask); // Lo mismo para el botón de eliminar
 /* Lo que hace, es que cuando el usuario hace click en cualquiera de los dos botones, se ejecutará
-la función correspondiente que se le haya asignado. */ 
-/* ***************************** */ 
+la función correspondiente que se le haya asignado. */
+/* ***************************** */
 
 
-/* ***************************** */ 
+/* ***************************** */
 function addTask() {
     const task = prompt("Agrega una tarea"); // Le pide al usuario agregar una tarea la cual se alojará en task
     if (task) { // Si task es verdadero, ejecutará el siguiente código
         tasks.push(task); // Agrega task al array tasks
-        statusInfo.textContent = `La última tarea agregada fue ${tasks[tasks.length - 1]}`; // Actualiza el texto de el elemento status-info con la última tarea agregada
+        statusInfo.textContent = `La última tarea agregada fue: ${tasks[tasks.length - 1]}`; // Actualiza el texto de el elemento status-info con la última tarea agregada
     } else { // Si task es falso, ejecutará el siguiente código
         statusInfo.textContent = "Valor incorrecto"; // Actualiza el texto de el elemento status-info con un mensaje de error
     }
     showTasks(); // Llama a la función showTasks() para actualizar la lista con la nueva información
 }
-/* ***************************** */ 
+/* ***************************** */
 
 
-/* ***************************** */ 
+/* ***************************** */
 function deleteTask() {
-    const task = prompt("Qué tarea desea eliminar?"); // Le pregunta al usuario que tarea desea eliminar y la aloja en task
-    const index = tasks.indexOf(task); // Aloja el índice de task en en la constante index con el método .indexOf()
-    if (index !== -1) { // Si el valor de index es distinto de -1, es decir, el valor de index se encuentra en el array tasks, será verdadero
-        const removed = tasks.splice(index, 1); // Remueve el elemento con el índice que aloje la constante index
-        statusInfo.textContent = `La última tarea eliminada fue ${removed}`; // Actualiza el texto de el elemento status-info con la última tarea removida
-    } else { 
-        statusInfo.textContent = "Valor incorrecto"; // Actualiza el texto de el elemento status-info con un mensaje de error
+    const taskIndex = parseInt(prompt("Ingrese el número de la tarea que desea eliminar:")); // Le pregunta al usuario el número de la tarea que desea eliminar y lo aloja en la variable taskIndex
+    if (!isNaN(taskIndex) && taskIndex >= 0 && taskIndex < tasks.length) { // Verifica si el valor ingresado es un número válido y está dentro del rango de índices del array tasks
+        const removed = tasks.splice(taskIndex - 1, 1); // Remueve el elemento en el índice especificado por el usuario y se le resta 1 a "taskIndex" para que el índice coincida con el utilizado por JavaScript.
+        statusInfo.textContent = `La tarea eliminada fue: "${removed}"`; // Actualiza el texto del elemento status-info con la tarea eliminada
+    } else {
+        statusInfo.textContent = "Índice incorrecto"; // Actualiza el texto del elemento status-info con un mensaje de error
     }
     showTasks(); // Llama a la función showTasks() para actualizar la lista con la nueva información
 }
-/* ***************************** */ 
+
+/* ***************************** */
 
 
-/* ***************************** */ 
+/* ***************************** */
 function showTasks() {
     ulContainer.innerHTML = ""; // Reinicia lo que contiene el cl contenedor con ID "ul-container" en el HTML
 
@@ -60,5 +60,5 @@ function showTasks() {
         ulContainer.appendChild(li); // Se agrega el elemento li como hijo de el elemento ul, el cual, tiene como ID "ul-container".
     });
 }
-/* ***************************** */ 
+/* ***************************** */
 
